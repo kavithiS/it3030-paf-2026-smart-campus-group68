8748c9c53132f1f00153009af22138b45c79cd26
Ramudi Jayalath <166049271+RamudiJ@users.noreply.github.com>
2026-04-24 18:45:32 +0530

    Updated Backend -Booking.java

--- backend/src/main/java/com/sliit/paf/model/Booking.java
+++ backend/src/main/java/com/sliit/paf/model/Booking.java
@@ -4,10 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Document(collection = "bookings")
 @Data
@@ -19,18 +22,18 @@ public class Booking {
     @Id
     private String id;
 
-    private Resource resource; // embedded document
-
+    private String resourceId;
+    private String resourceName;
     private String userId;
-
-    private LocalDateTime startTime;
-
-    private LocalDateTime endTime;
-
+    private String userName;
+    private LocalDate bookingDate;
+    private LocalTime startTime;
+    private LocalTime endTime;
     private String purpose;
-    private Integer expectedAttendees;
-
+    private Integer attendees;
     private BookingStatus status;
+    private String decisionReason;
 
-    private String adminReason;
+    @CreatedDate
+    private LocalDateTime createdAt;
 }
